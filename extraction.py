89b7aa46d6d297c21aac5fd9fb97ee20f1848ed3
alexanderkuebel <alexander.kuebel@code.berlin>
2021-08-23 21:07:15 +0200

    add data filtering option

--- extraction.py
+++ extraction.py
@@ -5,9 +5,27 @@ def extraction(url):
     data = pd.read_json(f"{url}")
 
     data = data[
-        ["name", "id", "description", "homepage", "zipcode", "email", "employeeCount"]
+        [
+            "name",
+            "id",
+            "description",
+            "homepage",
+            "zipcode",
+            "email",
+            "employeeCount",
+        ]
     ]
-    
+
+    # Cleaning dataframe of the NaNs at the employeeCount
+    data["employeeCount"] = data["employeeCount"].fillna("0")
+
+    # Sort the dataframe descending from the number of employees
+    data = data.sort_values(by=["employeeCount"], ascending=False)
+
+    # Search and sort for a specific keyword
+    # data["keyword"] = data["description"].str.find("web", 3)
+    # data = data.sort_values(by=["keyword"], ascending=False)
+
     return data
 
 
